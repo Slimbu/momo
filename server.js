cd50a066e68bd31684946125cfc72221b527745a
Slimbu <up811334@myport.ac.uk>
2022-02-24 18:05:46 +0000

    add cart page and auth

--- server.js
+++ server.js
@@ -5,16 +5,28 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+// Set Templete engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 // Assets
 app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render("home")
 })
 
-// Set Templete engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
