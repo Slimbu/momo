d46ff6f56537fcb41eb856b8752c6398ade7d657
Slimbu <up811334@myport.ac.uk>
2022-02-24 01:58:01 +0000

    make home page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,1 @@
-console.log("hello appjs")
+console.log("hello appj")
--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+// Assets
+app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render("home")
 })
 
+// Set Templete engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
