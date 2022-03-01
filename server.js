fc82afcb65762e71766dcefff049a624e059cf58
Slimbu <up811334@myport.ac.uk>
2022-03-01 12:51:55 +0000

    routes and auth

--- server.js
+++ server.js
@@ -10,23 +10,11 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+require('./routes/web')(app)
+
 // Assets
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-    res.render("home")
-})
 
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
